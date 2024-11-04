19abe9ffd8bd7e320d619dfbec4b9cfcde1ea42f
karinaerikads <karinaerikads48@gmail.com>
2024-11-04 16:16:16 -0300

    feat: adiciona interface ClienteRepository

---
+++ src/main/java/io/github/mscadastrocliente/mscadastrocliente/infra/repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package io.github.mscadastrocliente.mscadastrocliente.infra.repository;
+
+import io.github.mscadastrocliente.mscadastrocliente.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+}
