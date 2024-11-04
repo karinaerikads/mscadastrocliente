0f93b432afbb063be5c0addc8b144093c453f80d
karinaerikads <karinaerikads48@gmail.com>
2024-11-04 16:11:58 -0300

    feat: mapeia classes de dominio cliente e endereco

---
+++ src/main/java/io/github/mscadastrocliente/mscadastrocliente/domain/Cliente.java
@@ -0,0 +1,23 @@
+package io.github.mscadastrocliente.mscadastrocliente.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "cliente")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "endereco_id", referencedColumnName = "id")
+    private Endereco endereco;
+
+}
---
+++ src/main/java/io/github/mscadastrocliente/mscadastrocliente/domain/Endereco.java
@@ -0,0 +1,21 @@
+package io.github.mscadastrocliente.mscadastrocliente.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "endereco")
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rua;
+
+    private String cidade;
+
+    private String estado;
+}
