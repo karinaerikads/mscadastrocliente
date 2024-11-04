0fc835995222bec238a865de4112b729cefb4c75
karinaerikads <karinaerikads48@gmail.com>
2024-11-04 15:21:14 -0300

    primeiro commit

---
+++ src/main/java/io/github/mscadastrocliente/mscadastrocliente/MscadastroclienteApplication.java
@@ -0,0 +1,13 @@
+package io.github.mscadastrocliente.mscadastrocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscadastroclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscadastroclienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mscadastrocliente/mscadastrocliente/MscadastroclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mscadastrocliente.mscadastrocliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscadastroclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
