a7cfcd600eba1d9afc11b1b4af3d16c56a28e342
karinaerikads <karinaerikads48@gmail.com>
2024-11-05 17:39:59 -0300

    feat: adiciona domain Usuario para fazer autenticação

---
+++ src/main/java/io/github/mscadastrocliente/mscadastrocliente/domain/Usuario.java
@@ -0,0 +1,67 @@
+package io.github.mscadastrocliente.mscadastrocliente.domain;
+
+import io.github.mscadastrocliente.mscadastrocliente.user.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "usuario")
+public class Usuario implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String login;
+
+    @Column
+    private String password;
+
+    @Column
+    private UserRole role;
+
+    public Usuario(String login, String password, UserRole role){
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
