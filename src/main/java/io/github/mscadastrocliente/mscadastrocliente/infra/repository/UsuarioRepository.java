1706d79c3516634e6f420480c40ea3640d7a2147
karinaerikads <karinaerikads48@gmail.com>
2024-11-05 17:44:06 -0300

    feat: implementa repository de usuário com consulta e inserção de usuario

---
+++ src/main/java/io/github/mscadastrocliente/mscadastrocliente/infra/repository/UsuarioRepository.java
@@ -0,0 +1,22 @@
+package io.github.mscadastrocliente.mscadastrocliente.infra.repository;
+
+import io.github.mscadastrocliente.mscadastrocliente.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO usuario (login, password, role) VALUES (:#{#usuario.login}, :#{#usuario.password}, :#{#usuario.role})", nativeQuery = true)
+    void salvarUsuario(@Param("usuario") Usuario usuario);
+
+    @Query(value = "SELECT *FROM usuario WHERE login = :login", nativeQuery = true)
+    Usuario findByLogin(@Param("login") String login);
+
+}
